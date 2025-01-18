e330a7156aa2dd338fafb0c74147577cf199ea9f
1amAnonymous <mail2arkoprabho@gmail.com>
2025-01-19 02:46:19 +0530

    security leak repaired

--- utils/helper/aiREsponse.js
+++ utils/helper/aiREsponse.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyBwSZnlpZnHmgmpZhqLNWR5mqbetnUEcdI");
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function getResp(data){
